d414bc7cfc9fbb658477b13c75f81fc2383438ea
jakob. spiel .køhler <jakobspielkohler@live.dk>
2021-11-02 11:31:48 +0100

    tilføjet add create

---
+++ WebApp/Models/ProductValidationForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class ProductValidationForm
+    {
+        public int ProductId { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public int ManufacturerId { get; set; }
+    }
+}
