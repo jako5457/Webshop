f7e4f4a0e9b07f98cac236f166c14e3f8c27ffa2
jakob. spiel .køhler <jakobspielkohler@live.dk>
2021-11-03 08:38:27 +0100

    færdig med checkout

--- WebApp/Models/OrderModels.cs
+++ WebApp/Models/OrderModels.cs
@@ -23,6 +23,7 @@ namespace WebApp.Models
 
     public class CustomerModel
     {
+        public int customerId { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
         public string email { get; set; }
