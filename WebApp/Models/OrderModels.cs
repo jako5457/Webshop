a71e1fbcbddae79f1dcf849242cb37163dcdc45d
jakob. spiel .køhler <jakobspielkohler@live.dk>
2021-11-16 14:38:00 +0100

    tilføjet orders

---
+++ WebApp/Models/OrderModels.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApp.Models
+{
+    public class CreateOrderModel
+    {
+        public OrderModel Order { get; set; }
+        public List<ProductModel> Products { get; set; }
+    }
+
+    public class OrderModel
+    {
+        public long CustomerId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public long OrderId { get; set; }
+        public double TotalPrice { get; set; }
+    }
+}
